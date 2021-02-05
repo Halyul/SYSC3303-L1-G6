35693142cf304fc8a15d6a357bbe73d891a1f9ef
Lyam Milbury <71610885+Lyam-Milbury@users.noreply.github.com>
2021-02-05 15:19:24 -0500

    [Lyam M] feat: added message get function

--- project/src/project/Floor.java
+++ project/src/project/Floor.java
@@ -4,11 +4,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Floor implements Runnable {
 	
 	private int floorNumber;
 	private int topFloor;
+	private boolean elevatorPresent;
+	
 	private Communication c;
 	
 	private volatile ArrayList<byte[]> messages = new ArrayList<byte[]>();
@@ -39,7 +44,10 @@ public class Floor implements Runnable {
 		}
 	}
 	public void run() {
-
+		ReadInput("input.txt");
+		while(true) {
+			get();
+		}
 	}
 	
 	private class FloorButton {
@@ -47,11 +55,27 @@ public class Floor implements Runnable {
 	}
 	
 	private class FloorLamp {
-		private boolean currStatus = false;
 		
+		public FloorLamp() {
+			
+		}
+		
+		private boolean currStatus = false;
+		public void toggle() {
+			
+		}
 	}
 	
-	
+    private void get() {
+    	if (this.messages.size() != 0) {
+    		c.parse(messages.get(0));
+            messages.remove(0);
+            if (c.getFloor() == this.floorNumber) {
+            	
+            }
+    	}
+    }
+    
 	
 	private void send(long time, int direction, int CarButton, String state) {
 		Boolean isSent = false;
@@ -67,14 +91,20 @@ public class Floor implements Runnable {
 			while (inReader.hasNextLine()) {
 				String ins = inReader.nextLine();
 				String[] individualIns = ins.split("\\s+");
-				long convertedTime = Long.parseLong(individualIns[0].replaceAll("[^0-9]", ""));
+				SimpleDateFormat time = new SimpleDateFormat("HH:MM:SS.S");
 				int dir = 0;
 				if(individualIns[2].equals("Up"))
 					dir = 1;
 				else if(individualIns[2].equals("Down"))
 					dir = 0;
 				int destFloor = Integer.parseInt(individualIns[3]);
-				send(convertedTime, dir, destFloor, "reading");
+				try {
+					Date currTime = time.parse(individualIns[0]);
+					long epochTime = currTime.getTime();
+					send(epochTime, dir, destFloor, "Reading");
+				}	catch(ParseException e) {
+					e.printStackTrace();
+				}
 			}
 			
 			inReader.close();
