78b07401d0ce9e3465c69f542d2c83a21c730172
Lyam Milbury <71610885+Lyam-Milbury@users.noreply.github.com>
2021-02-06 00:19:18 -0500

    [Lyam] Docs: added comments pertinent to iteration 1

--- project/src/project/Floor.java
+++ project/src/project/Floor.java
@@ -9,10 +9,10 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class Floor implements Runnable {
-	
+	//Current floor Number
 	private int floorNumber;
+	//Number of the top floor
 	private int topFloor;
-	private boolean elevatorPresent;
 	
 	private Sender sender;
 	private Parser parser = new Parser();
@@ -25,16 +25,19 @@ public class Floor implements Runnable {
 		this.floorNumber = floorNumber;
 		this.topFloor = topFloor;
 		this.sender = new Sender(database);
+		//If bottom floor, initialize one set of up lamps and buttons
 		if(floorNumber == 0) {
 			FloorLamp upFLamp = new FloorLamp();
 			FloorButton upButton = new FloorButton();
 			DirectionLamp UpDirLamp = new DirectionLamp();
 		}
+		//If top floor, initialize one set of down lamps and buttons
 		else if(floorNumber == topFloor) {
 			FloorLamp downFLamp = new FloorLamp();
 			FloorButton downButton = new FloorButton();
 			DirectionLamp downDirLamp = new DirectionLamp();
 		}
+		//else, initialize up/down lamps and buttons
 		else {
 			FloorLamp upFLamp = new FloorLamp();
 			FloorLamp downFLamp = new FloorLamp();
@@ -46,6 +49,9 @@ public class Floor implements Runnable {
 			DirectionLamp downDirLamp = new DirectionLamp();
 		}
 	}
+    /**
+     * @see java.lang.Runnable#run()
+     */
 	public void run() {
 		ReadInput("input.txt");
 		while(true) {
@@ -68,27 +74,43 @@ public class Floor implements Runnable {
 			
 		}
 	}
-	
+	/**
+	 * Get message from the scheduler
+	 */
     private void get() {
+    	//If there is a message
     	if (this.messages.size() != 0) {
-    		this.parser.parse(messages.get(0));
+    		this.parser.parse(messages.get(0)); // Parse message
             messages.remove(0);
+            //If the elevator is going up
             if (parser.getFloor() > this.floorNumber) {
             	System.out.println("Direction lamp on floor "+this.floorNumber+" is pointing up");
             }
+            //else if the elevator is going down
             else if (parser.getFloor() < this.floorNumber) {
             	System.out.println("Direction lamp on floor "+this.floorNumber+" is down");
             }
+            //Else the elevator is at its destination
             else {
             	System.out.println("Elevator has arrived on floor "+this.floorNumber);
             }
     	}
     }
-    
+	/**
+     * For iteration 1, Scheduler sends the message to here
+     * 
+     * @param inputMessage the message
+     */
     public void put(byte[] inputMessage) {
     	this.messages.add(inputMessage);
     }
-    
+    /**
+     * Send the message to the Scheduler
+     * @param time the time from the input file in epoch seconds
+     * @param direction the direction the elevator is going (up/down)
+     * @param CarButton The destination floor chosen by the passenger
+     * @param state the status of the floor 
+     */
 	private void send(long time, int direction, int CarButton, String state) {
 		Boolean isSent = false;
 		while(!isSent) {
@@ -96,20 +118,28 @@ public class Floor implements Runnable {
 		}
 	}
 	
+    /**
+     * Reads an input file containing the time, floor, direction and destination 
+     * of a passenger calling an elevator from a floor
+     * @param inputFile name of the input file
+     */
 	public void ReadInput(String inputFile) {
+		//Try opening the file of name inputFile
 		try {
 			File inFile = new File(inputFile);
 			Scanner inReader = new Scanner(inFile);
-			while (inReader.hasNextLine()) {
-				String ins = inReader.nextLine();
-				String[] individualIns = ins.split("\\s+");
-				SimpleDateFormat time = new SimpleDateFormat("HH:MM:SS.S");
+			//While the text file has another line
+			while (inReader.hasNextLine()) {		
+				String ins = inReader.nextLine();		//Goes to the next line, storing the current line in ins
+				String[] individualIns = ins.split("\\s+");		//Split the instructions at whitespace characters
 				int dir = 0;
-				if(individualIns[2].equals("Up"))
+				if(individualIns[2].equals("Up"))	//If passengers wants to go up
 					dir = 1;
-				else if(individualIns[2].equals("Down"))
+				else if(individualIns[2].equals("Down")) //If passengers wants to go down
 					dir = 0;
-				int destFloor = Integer.parseInt(individualIns[3]);
+				int destFloor = Integer.parseInt(individualIns[3]);	//Stores destination floor 
+				SimpleDateFormat time = new SimpleDateFormat("HH:MM:SS.S");		//Used for epoch time conversion
+				//Try parsing the time and converting it to epoch time
 				try {
 					Date currTime = time.parse(individualIns[0]);
 					long epochTime = currTime.getTime();
