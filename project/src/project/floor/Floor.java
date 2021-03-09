34f97cbaae038a7f1c84642db7ae119002b6e776
Lyam Milbury <71610885+Lyam-Milbury@users.noreply.github.com>
2021-03-08 22:02:30 -0500

    [Lyam M] Feat: Added floorTime to convert inputted time to seconds

--- project/src/project/floor/Floor.java
+++ project/src/project/floor/Floor.java
@@ -1,5 +1,6 @@
 package project.floor;
 
+import java.net.InetAddress;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -17,31 +18,42 @@ import java.time.ZoneOffset;
 
 import project.utils.*;
 
+/*
+ * 
+ * 
+ * */
+
 public class Floor implements Runnable {
 	//Current floor Number
 	private int floorNumber;
 	//Number of the top floor
 	private int topFloor;
-	
+	private double baseTime = 0.0;
+
 	private Sender sender;
 	private Parser parser = new Parser();
+
+	private InetAddress schedulerAddress;
+	private int schedulerPort;
 	
 	private volatile ArrayList<byte[]> messages = new ArrayList<byte[]>();
+	private volatile ArrayList<Integer> floorQueue;	//0 if passenger wants to go down, 1 if up
 	private FloorButton upButton;
 	private FloorButton downButton;
 	private DirectionLamp upLamp;
 	private DirectionLamp downLamp;
-
+	
 	/**
-	 * Floor constructor 
+	 * Floor constructor
 	 * @param floorNumber number of the current floor
-	 * @param topFloor number of the top floor 
+	 * @param topFloor number of the top floor
 	 * @param database database where messages are sent
 	 */
-	public Floor(int floorNumber, int topFloor, Database database) {
+	public Floor(int floorNumber, int topFloor, Database database, InetAddress schedulerAddress, int port) {
 		this.floorNumber = floorNumber;
 		this.topFloor = topFloor;
 		this.sender = new Sender(database);
+		this.floorQueue = new ArrayList<Integer>();
 		//If bottom floor, initialize one set of up lamps and buttons
 		if(floorNumber == 0) {
 			this.upButton = new FloorButton();
@@ -55,13 +67,15 @@ public class Floor implements Runnable {
 			this.upButton = new FloorButton();
 			this.downButton = new FloorButton();
 		}
-		
+
 		this.upLamp = new DirectionLamp();
 		this.downLamp = new DirectionLamp();
+		this.schedulerAddress = schedulerAddress;
+		this.schedulerPort = port;
 	}
-    /**
-     * @see java.lang.Runnable#run()
-     */
+	/**
+	 * @see java.lang.Runnable#run()
+	 */
 	public void run() {
 		ReadInput("floorInput.txt");
 		while(true) {
@@ -72,64 +86,64 @@ public class Floor implements Runnable {
 	/**
 	 * Get receives messages from the scheduler
 	 */
-    private void get() {
-    	//If there is a message
-    	if (this.messages.size() != 0) {
-    		this.parser.parse(messages.get(0)); // Parse message
-            messages.remove(0);
-            //if elevator reaches destination floor
-        if (parser.getRole().equals("Elevator") && parser.getState().equals("Stop") && parser.getFloor() == this.floorNumber) {
-			//If the floor is neither the top or bottom floor
-        	if(this.floorNumber != topFloor && this.floorNumber != 0) {
-        		//Turn off both button lights
-    			this.upButton.off();
-    			this.downButton.off();
+	private void get() {
+		//If there is a message
+		if (this.messages.size() != 0) {
+			this.parser.parse(messages.get(0)); // Parse message
+			messages.remove(0);
+			//if elevator reaches destination floor
+			if (parser.getRole().equals("Elevator") && parser.getState().equals("Stop") && parser.getFloor() == this.floorNumber) {
+				//If the floor is neither the top or bottom floor
+				if(this.floorNumber != topFloor && this.floorNumber != 0) {
+					//Turn off both button lights
+					this.upButton.off();
+					this.downButton.off();
+				}
+				//else if it is the top floor,
+				else if(this.floorNumber == topFloor) {
+					//turn off downButton light
+					this.downButton.off();
+				}
+				//elseif it is the bottom floor
+				else if(this.floorNumber == 0) {
+					//turn off upButton light
+					this.upButton.off();
+				}
 			}
-        	//else if it is the top floor,
-			else if(this.floorNumber == topFloor) {
-				//turn off downButton light
-				this.downButton.off();
+			//Following ifs set the direction of the DirectionLamp
+			if(parser.getDirection() == 1) {
+				upLamp.on();
+				downLamp.off();
 			}
-        	//elseif it is the bottom floor
-			else if(this.floorNumber == 0) {
-				//turn off upButton light
-				this.upButton.off();
+			else if(parser.getDirection() == 0) {
+				upLamp.off();
+				downLamp.on();
+			}
+			else {
+				upLamp.off();
+				downLamp.off();
 			}
 		}
-        //Following ifs set the direction of the DirectionLamp
-        if(parser.getDirection() == 1) {
-        	upLamp.on();
-        	downLamp.off();
-        }
-        else if(parser.getDirection() == 0) {
-        	upLamp.off();
-        	downLamp.on();
-        }
-        else {
-        	upLamp.off();
-        	downLamp.off();
-        }
-    }
-}
-    
-    
+	}
+
+
 	/**
-     * For iteration 1, Scheduler sends the message to here
-     * 
-     * @param inputMessage the message
-     */
-    public void put(byte[] inputMessage) {
-    	this.messages.add(inputMessage);
-    }
-    /**
-     * Send the message to the Scheduler
-     * @param time the time from the input file in epoch seconds
-     * @param currentFloor current floor number
-     * @param direction the direction the elevator is going (up/down)
-     * @param CarButton The destination floor chosen by the passenger
-     * @param state the status of the floor 
-     */
-    private void send(long time, int currentFloor, int direction, int CarButton, String state) {
+	 * For iteration 1, Scheduler sends the message to here
+	 *
+	 * @param inputMessage the message
+	 */
+	public void put(byte[] inputMessage) {
+		this.messages.add(inputMessage);
+	}
+	/**
+	 * Send the message to the Scheduler
+	 * @param time the time from the input file in epoch seconds
+	 * @param currentFloor current floor number
+	 * @param direction the direction the elevator is going (up/down)
+	 * @param CarButton The destination floor chosen by the passenger
+	 * @param state the status of the floor
+	 */
+	private void send(long time, int currentFloor, int direction, int CarButton, String state) {
 		Boolean isSent = false;
 		// Haoyu Xu: updated Sender
 		// role:Floor;id:<current floor number>;state:<your own definition>;direction:<1/up or 0/down>;floor:<button pressed in the car>;time:<time>
@@ -139,58 +153,106 @@ public class Floor implements Runnable {
 //		}
 	}
 	
-    /**
-     * Reads an input file containing the time, floor, direction and destination 
-     * of a passenger calling an elevator from a floor
-     * @param inputFile name of the input file
-     */
+	
+	/*
+	 * Used to convert input button presses to a reasonable time
+	 * @param inputTime the time when a button is pressed
+	 * */
+	private double floorTime(Date inputTime) {
+		String time = inputTime.toString();
+		double hour = Double.parseDouble(time.substring(0,2));
+		double minute = Double.parseDouble(time.substring(3,6));
+		double seconds = Double.parseDouble(time.substring(7,10));
+		
+		return (hour * 60) + minute + (seconds / 100); //Returns the time in a HHMM.SS format in seconds
+	}
+	
+	/**
+	 * Reads an input file containing the time, floor, direction and destination
+	 * of a passenger calling an elevator from a floor
+	 * @param inputFile name of the input file
+	 */
 	public void ReadInput(String inputFile) {
-		//Try opening the file of name inputFile
 		try {
 			File inFile = new File(inputFile);
 			Scanner inReader = new Scanner(inFile);
 			//While the text file has another line
-			while (inReader.hasNextLine()) {		
+			while (inReader.hasNextLine()) {
 				String ins = inReader.nextLine();		//Goes to the next line, storing the current line in ins
 				String[] individualIns = ins.split("\\s+");		//Split the instructions at whitespace characters
 				int dir = 0;
-				if(individualIns[2].equals("Up")) {	//If passengers wants to go up
-					dir = 1;
-					if(this.floorNumber != topFloor) {
-						if(!this.upButton.getState()) {
-							//Checks if the button is already on
-							this.upButton.on();
-						}
-						
-					}
-				}
-				else if(individualIns[2].equals("Down")) { //If passengers wants to go down
-					dir = 0;
-					if(this.floorNumber != 0) {
-						if(!this.downButton.getState()) {	//Checks if the button is already on
-							this.downButton.on();
-						}
-
-					}
-				}
 				int currentFloor = Integer.parseInt(individualIns[1]); // get the floor the user currently at
-				int destFloor = Integer.parseInt(individualIns[3]);	//Stores destination floor 
 				SimpleDateFormat time = new SimpleDateFormat("HH:MM:SS.S");	//Used for epoch time conversion
-				//Try parsing the time and converting it to epoch time
 				try {
 					Date currTime = time.parse(individualIns[0]);
-					LocalDate currDate = LocalDate.now();
-					LocalTime baseTime = LocalTime.MIDNIGHT;
-					long epochTime = currTime.getTime() + currDate.toEpochSecond(baseTime, ZoneOffset.UTC);
-					send(epochTime, currentFloor, dir, destFloor, "Reading");
+					double inputTime = floorTime(currTime);
+					if(baseTime == 0.0) {
+						baseTime = inputTime;
+					}
+					//TODO: Send message and add people to queue based on time
 				}	catch(ParseException e) {
-					e.printStackTrace();
+					
 				}
+				
+
 			}
-			
 			inReader.close();
-		}	catch (FileNotFoundException e) {
+		}	catch (FileNotFoundException | InterruptedException e) {
 			System.out.println(Thread.currentThread().getName() + ": File not found");
 		}
+		
+		
+		
+		//Legacy code
+//		try {
+//			File inFile = new File(inputFile);
+//			Scanner inReader = new Scanner(inFile);
+//			//While the text file has another line
+//			while (inReader.hasNextLine()) {
+//				String ins = inReader.nextLine();		//Goes to the next line, storing the current line in ins
+//				String[] individualIns = ins.split("\\s+");		//Split the instructions at whitespace characters
+//				int dir = 0;
+//				int currentFloor = Integer.parseInt(individualIns[1]); // get the floor the user currently at
+//				if(individualIns[2].equals("Up")) {	//If passengers wants to go up
+//					dir = 1;
+//					if(this.floorNumber != topFloor) {
+//						if(!this.upButton.getState()) {
+//							//Checks if the button is already on
+//							this.upButton.on();
+//						}
+//					}
+//				}
+//				else if(individualIns[2].equals("Down")) { //If passengers wants to go down
+//					dir = 0;
+//					if(this.floorNumber != 0) {
+//						if(!this.downButton.getState()) {	//Checks if the button is already on
+//							this.downButton.on();
+//						}
+//					}
+//				}
+//				if(currentFloor == this.floorNumber)
+//					floorQueue.add(dir);
+//				int destFloor = Integer.parseInt(individualIns[3]);	//Stores destination floor
+//				SimpleDateFormat time = new SimpleDateFormat("HH:MM:SS.S");	//Used for epoch time conversion
+//				//Try parsing the time and converting it to epoch time
+//				try {
+//					Date currTime = time.parse(individualIns[0]);
+//					LocalDate currDate = LocalDate.now();
+//					LocalTime baseTime = LocalTime.MIDNIGHT;
+//					long epochTime = currTime.getTime() + currDate.toEpochSecond(baseTime, ZoneOffset.UTC);
+//					send(epochTime, currentFloor, dir, destFloor, "Reading");
+//				}	catch(ParseException e) {
+//					e.printStackTrace();
+//				}
+//				Thread.sleep(1000);
+//			}
+//			inReader.close();
+//		}	catch (FileNotFoundException | InterruptedException e) {
+//			System.out.println(Thread.currentThread().getName() + ": File not found");
+//		}
+	}
+	
+	public static void main(String args[]) {
+		
 	}
 }
