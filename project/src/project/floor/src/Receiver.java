04e9c389acaf09630e25ba094e4d1a1f1fbe193f
Halyul <i@halyul.dev>
2021-03-13 16:01:52 -0500

    [Haoyu Xu] docs: update README.txt

--- project/src/project/floor/src/Receiver.java
+++ project/src/project/floor/src/Receiver.java
@@ -5,11 +5,11 @@ import java.io.*;
 import java.net.*;
 
 public class Receiver implements Runnable {
-	private InetAddress schedulerAddress;
 	private Floor floor;
-    private int schedulerPort;
     private DatagramSocket receiveSocket;
     private DatagramPacket receivePacket;
+    private boolean isDebug;
+    private byte[] debugMessage;
     
     public Receiver(Floor floor, int port) {
     	this.floor = floor;
@@ -21,6 +21,13 @@ public class Receiver implements Runnable {
 		}
     }
     
+    public Receiver(boolean isDebug) {
+		this.isDebug = isDebug;
+	}
+    
+    /**
+	 * Receive a message
+	 */
 	private void execute() {
 		byte data[] = new byte[5000];
 	    this.receivePacket = new DatagramPacket(data, data.length);
@@ -34,13 +41,32 @@ public class Receiver implements Runnable {
 	    parse();
 	}
 	
+	/**
+	 * Parse a message
+	 */
 	private void parse() {
 		int messageLength = receivePacket.getLength();
 		byte[] message = new byte[messageLength];
 		if (messageLength >= 0)
 			System.arraycopy(receivePacket.getData(), 0, message, 0, messageLength);        // Intercept the required part
 		System.out.println(new String(message));
-		floor.put(message);
+		if (!this.isDebug) {
+			floor.put(message);
+		} else {
+			this.debugMessage = message;
+		}
+	}
+	
+	/**
+	 * For unit test only, generate a new packet from messageBytes and parse it
+	 * @param messageBytes the input message
+	 * @return parse message
+	 */
+	public byte[] debug(byte[] messageBytes) {
+		this.receivePacket = new DatagramPacket(messageBytes, messageBytes.length);
+		parse();
+		return this.debugMessage;
+		
 	}
     
     @Override 
