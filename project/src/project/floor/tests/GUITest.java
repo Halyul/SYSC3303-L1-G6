118e9f8a81280e4ba2d7c9f2041af7b152b42b2f
Lyam Milbury <71610885+Lyam-Milbury@users.noreply.github.com>
2021-04-13 18:05:25 -0400

    [Lyam] feat: add GUI JUnit tests

---
+++ project/src/project/floor/tests/GUITest.java
@@ -0,0 +1,48 @@
+package project.floor.tests;
+
+import org.junit.jupiter.api.*;
+
+import project.floor.src.GUI;
+
+public class GUITest {
+	private GUI g;
+	@BeforeEach
+	public void setUp() throws Exception{
+		g = new GUI(1);
+	}
+	
+	@SuppressWarnings("static-access")
+	@Test
+    @DisplayName("Updates the JLabels in the GUI by changing floor and direction")   
+	public void updateNewFloor() {
+        String message = "role:Elevator;id:1;state:Moving;floor:4;direction:1;time:0;";
+        byte[] messageBytes = message.getBytes();
+		g.put(messageBytes);
+		g.updateGUI();
+		Assertions.assertEquals(g.currLabel(0, 0), "4");
+		Assertions.assertEquals(g.currLabel(0, 1), "Moving");
+		Assertions.assertEquals(g.currLabel(0, 2), "Up");
+		
+		message = "role:Elevator;id:1;state:Moving;floor:5;direction:0;time:0;";
+		messageBytes = message.getBytes();
+		g.put(messageBytes);
+		g.updateGUI();
+		Assertions.assertEquals(g.currLabel(0, 0), "5");
+		Assertions.assertEquals(g.currLabel(0, 1), "Moving");
+		Assertions.assertEquals(g.currLabel(0, 2), "Down");
+	}
+	
+	@SuppressWarnings("static-access")
+	@Test
+    @DisplayName("Updates the JLabels in the GUI by sending error and changing states")   
+	public void updateError() {
+        String message = "role:Elevator;id:1;error:stuckBetweenFloors;floor:2;time:0;state:Error";
+        byte[] messageBytes = message.getBytes();
+		g.put(messageBytes);
+		g.updateGUI();
+		Assertions.assertEquals(g.currLabel(0, 1), "Error");
+		Assertions.assertEquals(g.currLabel(0, 3), "Elevator is stuck between two floors");
+
+	}
+
+}
