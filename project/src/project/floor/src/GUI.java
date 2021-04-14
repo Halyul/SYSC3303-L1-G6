9a81f2b5d23951a8030e98770bb4134049c96302
Lyam Milbury <71610885+Lyam-Milbury@users.noreply.github.com>
2021-04-14 15:05:02 -0400

    [Lyam] Docs: Added additional comments

--- project/src/project/floor/src/GUI.java
+++ project/src/project/floor/src/GUI.java
@@ -13,7 +13,9 @@ public class GUI implements Runnable {
 	//Array containing JLabels to be changed via info, each elevators respective info
 	//is stored at index (elevatorNumber-1)*4 +1, 2, 3
 	private volatile ArrayList<JLabel> ElevatorInfo = new ArrayList<JLabel>();
+	//Stores messages received from scheduler
 	private volatile static ArrayList<byte[]> messages = new ArrayList<byte[]>();
+	//Stores previous direction of elevator
 	private volatile ArrayList<Integer> prevDir = new ArrayList<Integer>();
 	
 	private Parser parser = new Parser();
@@ -23,19 +25,19 @@ public class GUI implements Runnable {
 	 */
 	public GUI(int numberOfElevators) {
     	JFrame frame = new JFrame("Concierge System");
-    	frame.setLayout(new GridLayout(numberOfElevators+1, 5));
-    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	frame.setSize(800,500);
-    	frame.setVisible(true);
+    	frame.setLayout(new GridLayout(numberOfElevators+1, 5));	//Creates rows based on number of elevators
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		//Close program on clicking X
+    	frame.setSize(800,500);		//Set size
+    	frame.setVisible(true);		//Makes GUI visible
     	for(int i = 0; i <= numberOfElevators; i++) {
-    		if(i == 0) {
+    		if(i == 0) {	//If first row, add titles
         		frame.add(new JLabel("Elevator Number"));
         		frame.add(new JLabel("Current Floor"));
         		frame.add(new JLabel("State"));
         		frame.add(new JLabel("Direction"));
         		frame.add(new JLabel("Error"));
     		}
-    		else {
+    		else {	//Else, add pertinent information for each elevator
         		JLabel elevatorNum = new JLabel("Elevator " + i);
         		JLabel currFloor = new JLabel("1");
         		JLabel currState = new JLabel("Idle");
@@ -61,7 +63,13 @@ public class GUI implements Runnable {
 	public static void put(byte[] inputMessage) {
 		messages.add(inputMessage);
 	}
-	
+	/*
+	 * currLabel() Used to select wanted string from jlabel
+	 * for JUnit test
+	 * @param elevatorNum elevator identifier
+	 * @param wantedText 0 if floor, 1 if state, 2 if Direction, 3 if error
+	 * @return String from JLabel
+	 */
 	public String currLabel(int elevatorNum, int wantedText) {
 		switch(wantedText) {
 			case 0:	//Floor
@@ -107,17 +115,17 @@ public class GUI implements Runnable {
 	 * to the GUI in the message list, updates JLabels to reflect this info
 	 */
 	public synchronized void updateGUI() {
-		if(messages.size() != 0) {
-			this.parser.parse(messages.get(0));
-			messages.remove(0);
-			if(parser.getRole().equals("Elevator")) {
+		if(messages.size() != 0) {	//If message in queue
+			this.parser.parse(messages.get(0));	//Parse
+			messages.remove(0);	//remove message from queue
+			if(parser.getRole().equals("Elevator")) {	//If message is from elevator
 				int elevatorNum = parser.getIdentifier() - 1;
 				int elevatorDir = parser.getDirection();
 				String readableDir = "";
 				
 				//Set current floor number
-				this.ElevatorInfo.get(elevatorNum*4).setText(""+parser.getFloor());
-				this.ElevatorInfo.get((elevatorNum*4)+1).setText(""+parser.getState());
+				this.ElevatorInfo.get(elevatorNum*4).setText(""+parser.getFloor());	//update floor
+				this.ElevatorInfo.get((elevatorNum*4)+1).setText(""+parser.getState());	//update state
 
 				if(this.prevDir.get(elevatorNum) != elevatorDir) {	//If direction of the elevator has changed
 					switch(elevatorDir) {
@@ -134,8 +142,8 @@ public class GUI implements Runnable {
 					this.prevDir.set(elevatorNum, elevatorDir);
 					this.ElevatorInfo.get((elevatorNum*4)+2).setText(readableDir);	//Update direction on GUI
 				}
-				
-				if(parser.getState().equals("Error")) {
+				//If the message contains an error
+				if(parser.getState().equals("Error")) {	
 					//Set error
 					this.ElevatorInfo.get((elevatorNum*4)+3).setText(resolveError(parser.getError()));
 				}
