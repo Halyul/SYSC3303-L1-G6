a17ad7ba1f3e760c6efc0f449dfe9cfb20ab3fbc
Lyam Milbury <71610885+Lyam-Milbury@users.noreply.github.com>
2021-04-14 14:56:48 -0400

    [Lyam] Docs: added comments

--- project/src/project/floor/src/FloorButton.java
+++ project/src/project/floor/src/FloorButton.java
@@ -5,29 +5,41 @@ public class FloorButton {
 	private boolean FloorLamp = false;
 	private int floor;
 	private boolean dir;	//True for up, false for down
-	
+	/**
+	*	FloorButton constructor
+	*	@param floor floor which the floor buttons are found at
+	*	@param dir true if up, false if down
+	*/
 	public FloorButton(int floor, boolean dir) {
 		this.floor = floor;
 		this.dir = dir;
 	}
     /**
-     * Turn on the light
+     * on() used to turn on the floor light
      */
     public void on() {
         this.FloorLamp = true;
     }
     
     /**
-     * Turn off the light
+     * off() used to turn off the floor light
      */
     public void off() {
         this.FloorLamp = false;
     }
-    
+    /**
+     * getDirection() used to get the direction of the floor button
+     *
+     * @return direction of the button
+     */
     public boolean getDirection(){
     	return this.dir;
     }
-    
+    /**
+     * getFloor() used to get the floor where the floor button is
+     *
+     * @return floor of the floor button
+     */
     public int getFloor() {
     	return this.floor;
     }
